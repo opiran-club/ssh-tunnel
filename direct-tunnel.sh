43f2de237bb5be042a83339b8571ac7666fb49b0
Opiran club <130220895+opiran-club@users.noreply.github.com>
2023-08-25 23:33:20 +0330

    Update direct-tunnel.sh

--- direct-tunnel.sh
+++ direct-tunnel.sh
@@ -5,20 +5,54 @@ CYAN="\e[36m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
 RED="\e[31m"
-NC="\e[0m" # No Color
+BLUE="\e[34m"
+NC="\e[0m"
+WHITE="\e[0m"
 
+# Function to freeze title
+freeze_title() {
+    echo -ne "\033[0;0H"
+}
+
+# Function to continue after pressing Enter
+press_enter() {
+    echo -e "\n ${RED}Press Enter to continue... ${NC}"
+    read
+}
+
+# Check if script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# Trap Ctrl+C to freeze title
+trap freeze_title SIGINT
+
+# Clear the screen and freeze the title
 clear
+echo -e "${BLUE}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+echo -e "          ${YELLOW}Create By${NC} ${GREEN} OPIran${NC}"
+echo -e "             ${YELLOW}TG-Group${NC} ${GREEN} @OPIranCluB${NC}"
+echo -e "${BLUE}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+
 # Prompt for port_config_kharej
-read -p "${CYAN}Enter the port for config_kharej:${NC} " port_config_kharej
+read -p "${CYAN}Enter the destination port (SSH / V2ray) (service on your foreign VPS):${NC} " port_config_kharej
 
 # Prompt for ip_kharej
-read -p "${CYAN}Enter the IP address for ip_kharej:${NC} " ip_kharej
+read -p "${CYAN}Enter the destination IP address (2nd VPS or Foreign VPS):${NC} " ip_kharej
 
 # Prompt for port_tunnel
-read -p "${CYAN}Enter the port for tunnel:${NC} " port_tunnel
+read -p "${CYAN}Enter the source port for tunnel (Local VPS):${NC} " port_tunnel
+
+# Prompt for remote username
+read -p "${CYAN}Enter the destination username (e.g., root):${NC} " remote_user
+
+# Prompt for remote username
+read -p "${CYAN}Give me the path of this script (ex. /etc/direct-tunnel.sh):${NC} " path
 
 # Set up SSH tunnel command
-ssh_tunnel_command="ssh -N -L *:${port_tunnel}:localhost:${port_config_kharej} root@${ip_kharej}"
+ssh_tunnel_command="ssh -N -L *:${port_tunnel}:localhost:${port_config_kharej} ${remote_user}@${ip_kharej}"
 
 # Check if SSH key authentication is already set up
 if ! ssh-add -l &> /dev/null; then
@@ -28,6 +62,9 @@ if ! ssh-add -l &> /dev/null; then
         exit 1
     fi
 fi
+
+# Copy SSH key to remote server for passwordless authentication
+ssh-copy-id ${remote_user}@${ip_kharej}
 
 # Run the SSH tunnel command
 echo -e "${GREEN}Setting up SSH tunnel...${NC}"
@@ -40,13 +77,16 @@ cron_command="@reboot $ssh_tunnel_command"
 echo -e "${GREEN}SSH tunnel set up and added to cron job.${NC}"
 echo -e "${YELLOW}The SSH tunnel will be established on system startup.${NC}"
 
+# Allow the user to press Enter to continue
+press_enter
+
 # Systemd service definition
 echo "[Unit]
-Description=Setup SSH Tunnel on Startup
+Description=OPIran Setup SSH Tunnel on Startup
 After=network.target
 
 [Service]
-ExecStart=/path/to/setup_tunnel.sh
+ExecStart=${path}
 Restart=on-failure
 User=your_username
 
@@ -57,3 +97,6 @@ WantedBy=multi-user.target" > /etc/systemd/system/ssh-tunnel.service
 systemctl daemon-reload
 systemctl start ssh-tunnel
 systemctl enable ssh-tunnel
+
+# Allow the user to press Enter to continue
+press_enter
